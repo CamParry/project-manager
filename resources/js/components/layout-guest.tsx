43043415bfafdd45e67cf2eceb12fb6e07a3e89e
Cam Parry <64690031+CamParry@users.noreply.github.com>
2025-08-13 14:24:51 +0200

    Add text color to gust layout

--- resources/js/components/layout-guest.tsx
+++ resources/js/components/layout-guest.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from "react";
 
 export function GuestLayout({ children }: PropsWithChildren) {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center bg-bg-muted px-6">
+        <div className="flex min-h-screen flex-col items-center justify-center bg-bg-muted px-6 text-text">
             <div className="w-full max-w-md">{children}</div>
         </div>
     );
